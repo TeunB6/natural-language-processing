e7456b5154916e9059257fdbec83c707a8ede6cb
TeunB6 <t.boersma.4@student.rug.nl>
2026-02-22 20:52:52 +0100

    feat: improved updates for get model

--- src/training/train.py
+++ src/training/train.py
@@ -1,7 +1,8 @@
 from sklearn.base import BaseEstimator
 from src.data.data import AGNews
 
-from src.const import MODEL_DIR
+from src.const import MODEL_DIR, CONSOLE
+from rich.panel import Panel
 import pickle
 
 # This file contains functions to train and load simple baseline models on the AG News dataset.
@@ -22,6 +23,12 @@ def get_model(model: BaseEstimator, ds: AGNews) -> BaseEstimator:
     """Load a trained model from disk."""
     model_path = MODEL_DIR / f"{model.__class__.__name__}.pkl"
     if not model_path.exists():
+        CONSOLE.print(
+                Panel(
+                    f"No saved model found for {model.__class__.__name__}. Training a new model...",
+                    style="bold yellow",
+                )
+            )
         model = train_model(model, ds)
     with open(model_path, "rb") as f:
         model = pickle.load(f)
