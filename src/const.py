6f8cee5d99c6ae5e7c82f0ed9a7a253801906cc3
Marcus Persson <marcusper2003@gmail.com>
2026-03-08 23:12:14 +0100

    introduce a config because i got siked from retraining a model

--- src/const.py
+++ src/const.py
@@ -2,14 +2,19 @@ from pathlib import Path
 from rich.console import Console
 from src.utils.logging import Logger
 from torch.cuda import is_available as torch_cuda_available
+import yaml
+
+with open("config.yaml", "r") as config_file:
+    data = yaml.load(config_file, Loader=yaml.SafeLoader)
+    paths = data["paths"]
 
 ROOT_DIR = Path(__file__).parent.parent
-RANDOM_SEED = 33
-DATA_DIR = ROOT_DIR / Path("data/ag_news")
-MODEL_DIR = ROOT_DIR / Path("models")
-RESULTS_DIR = ROOT_DIR / Path("results")
+RANDOM_SEED = data["random_seed"]
+DATA_DIR = ROOT_DIR / Path(paths["data"])
+MODEL_DIR = ROOT_DIR / Path(paths["model"])
+RESULTS_DIR = ROOT_DIR / Path(paths["results"])
 DEVICE = "cuda" if torch_cuda_available() else "cpu"
-DEBUG = True
+DEBUG = data["debug"]
 CONSOLE = Console()
 LOGGER = Logger("nlp_pipeline")
-RETRAIN_MODEL = True
+RETRAIN_MODEL = data["retrain_model"]
